f710fdf3683bacfe106f167244277e04a3623d15
MAALONSO <miguel.coba@gnp.com.mx>
2020-08-21 11:08:44 -0500

    Iniciando proyecto

---
+++ src/main/java/com/example/dicionario/DiccionarioSegurosApplication.java
@@ -0,0 +1,13 @@
+package com.example.dicionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiccionarioSegurosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiccionarioSegurosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dicionario/DiccionarioSegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dicionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiccionarioSegurosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
