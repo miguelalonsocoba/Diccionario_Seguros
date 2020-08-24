e16f2e12b306cbe45aaa338c397d42ff15afb891
MAALONSO <miguel.coba@gnp.com.mx>
2020-08-24 17:28:52 -0500

    Aliminando clases

--- src/main/java/com/example/dicionario/controller/DiccionarioController.java
+++ src/main/java/com/example/dicionario/controller/DiccionarioController.java
@@ -1,14 +1,9 @@
 package com.example.dicionario.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.dicionario.dto.TerminoDTO;
-import com.example.dicionario.service.ITerminoService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,15 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping(path = "/diccionario")
 public class DiccionarioController {
-
-	@Autowired
-	@Qualifier(value = "TerminoService")
-	private ITerminoService service;
-
-	@GetMapping(path = "/getTermino")
-	public TerminoDTO getTermino(@RequestParam(name = "id") final Integer id) {
-		return service.findById(id);
-	}
 
 	/** Metodo de ejemplo. */
 	@GetMapping(path = "/message")
